3bb035bd04ab6769e7a23335762dff3fcc992322
Daniel Fang <qkfang@hotmail.com>
2025-11-14 16:23:09 +1100

    add

--- infra/entra-lab-provision-exec.ps1
+++ infra/entra-lab-provision-exec.ps1
@@ -4,8 +4,8 @@
 # sub-aiaaa-lab1
 # .\entra-lab-provision.ps1 ba15181f-9a45-4eff-9043-4ddf263b6dc2 aiapps.top lab1 310 319
 
-# sub-aiaaa-lab2
-# .\entra-lab-provision.ps1 22f484c3-b754-45aa-8cec-e40bb48bcb34 aiapps.top lab2 320 329
+sub-aiaaa-lab2
+.\entra-lab-provision.ps1 22f484c3-b754-45aa-8cec-e40bb48bcb34 aiapps.top lab2 320 329
 
 # sub-aiaaa-lab3
 .\entra-lab-provision.ps1 bb2b6250-daa2-4e16-8387-2fa677dc4d7f aiapps.top lab3 330 339
@@ -16,4 +16,13 @@
 # sub-aiaaa-lab5
 .\entra-lab-provision.ps1 bfa1eb08-61a0-41d1-99ef-f02f3286f537 aiapps.top lab5 350 359
 
+# sub-aiaaa-lab6
+.\entra-lab-provision.ps1 ba15181f-9a45-4eff-9043-4ddf263b6dc2 aiapps.top lab6 360 364
+.\entra-lab-provision.ps1 22f484c3-b754-45aa-8cec-e40bb48bcb34 aiapps.top lab6 365 369
 
+# sub-aiaaa-lab7
+.\entra-lab-provision.ps1 bb2b6250-daa2-4e16-8387-2fa677dc4d7f aiapps.top lab7 370 374
+.\entra-lab-provision.ps1 ac5c4d49-95c6-4b65-a273-5e3e562e7544 aiapps.top lab7 375 379
+
+# sub-aiaaa-lab8
+.\entra-lab-provision.ps1 bfa1eb08-61a0-41d1-99ef-f02f3286f537 aiapps.top lab8 380 389
