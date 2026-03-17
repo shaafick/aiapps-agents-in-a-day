eabaf40563b03403625f7b235b66d4a67eac248e
shaafick <79563232+shaafick@users.noreply.github.com>
2026-03-17 22:55:52 +0000

    save

--- labs/10-LLM/7a-openai-sdk.js
+++ labs/10-LLM/7a-openai-sdk.js
@@ -4,3 +4,17 @@ const client = new OpenAIClient(
   "https://aiaaa-s2-openai.openai.azure.com/",
   new AzureKeyCredential("8d91f222533647a488602ebc75e7a180")
 );
+
+// Block Reference 1
+const chatResponse = client.getChatCompletions("gpt-4.1", [
+  { role: "user", content: "What are the different types of road bikes?" },
+]);
+
+// Block Reference 2
+chatResponse
+  .then((result) => {
+    for (const choice of result.choices) {
+      console.log(choice.message.content);
+    }
+  })
+  .catch((err) => console.log(`Error: ${JSON.stringify(err)}`));
