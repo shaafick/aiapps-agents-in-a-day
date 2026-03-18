677a4a56be6ab2a1968b5fa9db4569519dc00de9
shaafick <79563232+shaafick@users.noreply.github.com>
2026-03-18 00:01:51 +0000

    save

--- labs/20-Chatbot/1b-import.js
+++ labs/20-Chatbot/1b-import.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+rrequire('dotenv').config();
 const { MongoClient } = require('mongodb');
 const fs = require('fs');
 const path = require('path');
@@ -14,10 +14,72 @@ async function main() {
         console.log('Connected to MongoDB');
         const db = client.db(dbname);
 
-        // TODO: Add product data loading code here
+        // Load product data
+        console.log("Loading product data");
+        // Initialize the product collection pointer (will automatically be created if it doesn't exist)
+        const productCollection = db.collection("products");
+
+        // Define the path to the local JSON file
+        const jsonFilePath = path.join("data", "product.json");
+
+        // Read the JSON file
+        const productRawData = fs.readFileSync(
+            path.join("data", "product.json"),
+            "utf8"
+        );
+        const productData = JSON.parse(productRawData).map((prod) =>
+            cleanData(prod)
+        );
+
+        // Delete any existing products
+        console.log("Deleting existing products");
+        await productCollection.deleteMany({});
+        var result = await productCollection.bulkWrite(
+            productData.map((product) => ({
+                insertOne: {
+                    document: product,
+                },
+            }))
+        );
+        console.log(`${result.insertedCount} products inserted`);
 
 
-        // TODO: Add customer and sales data loading code here
+
+
+        // Load customer and sales data
+        console.log("Retrieving combined Customer/Sales data");
+        const customerCollection = db.collection("customers");
+        const salesCollection = db.collection("sales");
+
+        const custSalesRawData = fs.readFileSync(
+            path.join("data", "custSalesData.json"),
+            "utf8"
+        );
+        const custSalesData = JSON.parse(custSalesRawData).map((custSales) =>
+            cleanData(custSales)
+        );
+
+        console.log("Split customer and sales data");
+        const customerData = custSalesData.filter(
+            (cust) => cust["type"] === "customer"
+        );
+        const salesData = custSalesData.filter(
+            (sales) => sales["type"] === "salesOrder"
+        );
+
+
+        console.log("Loading customer data");
+        await customerCollection.deleteMany({});
+        result = await customerCollection.insertMany(customerData);
+        console.log(`${result.insertedCount} customers inserted`);
+
+
+        console.log("Loading sales data");
+        await salesCollection.deleteMany({});
+        result = await salesCollection.insertMany(salesData);
+        console.log(`${result.insertedCount} sales inserted`);
+
+
 
 
     } catch (err) {
